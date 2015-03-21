60c569484025dbca35446b35a3b964edd887e55f
yahavi <yahav.itzhak@mail.huji.ac.il>
2015-03-21 18:11:16 +0200

    Ex3

--- app/src/main/java/il/ac/huji/todolist/ItemsAdapter.java
+++ app/src/main/java/il/ac/huji/todolist/ItemsAdapter.java
@@ -1,118 +1,84 @@
 package il.ac.huji.todolist;
 
-import android.app.Activity;
-import android.app.Dialog;
 import android.content.Context;
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Calendar;
 import java.util.List;
 
 
-public class ItemsAdapter extends ArrayAdapter<String> {
+public class ItemsAdapter extends ArrayAdapter<TodoItem> {
     final Context m_Context;
     final int m_Resource;
-    List<String> m_Objects;
+    List<TodoItem> m_Objects;
 
-    /**
-     * Inner class represents an item delete dialog
-     */
-    private class ItemDialog extends Dialog {
-        final int m_Position;
-        final String m_Name;
 
-        public ItemDialog(Activity activity, int position, String name){
-            super(activity);
-            m_Position = position;
-            m_Name = name;
-        }
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            TextView dialogName;
-            Button deleteButton;
-
-            super.onCreate(savedInstanceState);
-            requestWindowFeature(Window.FEATURE_NO_TITLE);
-            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
-            setContentView(R.layout.item_dialog);
-
-            dialogName = (TextView)findViewById(R.id.itemName);
-            dialogName.setText(m_Name);
-
-            deleteButton = (Button) findViewById(R.id.deleteButton);
-            deleteButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    m_Objects.remove(m_Position);
-                    notifyDataSetChanged();
-                    dismiss();
-                }
-            });
-        } // onCreate
-    } // ItemDialog
-
-    public ItemsAdapter(Context context, int resource, List<String> objects){
+    public ItemsAdapter(Context context, int resource, List<TodoItem> objects){
         super(context, resource, objects);
         m_Context = context;
         m_Resource = resource;
         m_Objects = objects;
+
+    }
+
+    private String getDateFormat(Calendar date){
+        String res = "";
+
+        res += date.get(Calendar.DAY_OF_MONTH) + "/"
+            +  (date.get(Calendar.MONTH) + 1) + "/"
+            +  date.get(Calendar.YEAR);
+        return res;
+    }
+
+    private static boolean isToday(Calendar date){
+        Calendar currentDate = Calendar.getInstance();
+        return (date.get(Calendar.DAY_OF_MONTH) == currentDate.get(Calendar.DAY_OF_MONTH) &&
+                date.get(Calendar.MONTH) == currentDate.get(Calendar.MONTH) &&
+                date.get(Calendar.YEAR) == currentDate.get(Calendar.YEAR));
     }
 
     /**
-     * Used when adding to the adapter
+     * Used in adding to the adapter
      */
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
         final View rawView;
-        final String item;
+        final TodoItem item;
 
         if (convertView == null) {
             LayoutInflater vi;
 
             vi = LayoutInflater.from(getContext());
-            rawView = vi.inflate(R.layout.list_raw, null);
+            rawView = vi.inflate(R.layout.list_raw, parent, false);
         } else {
             rawView = convertView;
         }
         item = getItem(position);
 
         if (item != null){
-            TextView textViewItem;
+            TextView tvItemName;
+            TextView tvItemDate;
 
-            textViewItem = (TextView) rawView.findViewById(R.id.item);
-            textViewItem.setText(item);
-            if (position % 2 == 1){
-                textViewItem.setTextColor(Color.BLUE);
+            tvItemName = (TextView) rawView.findViewById(R.id.txtTodoTitle);
+            tvItemDate = (TextView) rawView.findViewById(R.id.txtTodoDueDate);
+            tvItemName.setText(item.getItemName());
+            tvItemDate.setText(getDateFormat(item.getDate()));
+
+            Calendar currentDate = Calendar.getInstance();
+            if (item.getDate().after(currentDate) || isToday(item.getDate())){
+                tvItemDate.setTextColor(Color.BLACK);
+                tvItemName.setTextColor(Color.BLACK);
             } else {
-                textViewItem.setTextColor(Color.RED);
+                tvItemDate.setTextColor(Color.RED);
+                tvItemName.setTextColor(Color.RED);
             }
-
-            textViewItem.setOnLongClickListener(new View.OnLongClickListener() {
-
-                @Override
-                public boolean onLongClick(View v) {
-                    ItemDialog itemDialog;
-
-                    itemDialog = new ItemDialog((Activity) getContext(), position, item);
-                    itemDialog.getWindow().
-                            setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                    itemDialog.show();
-                    return false;
-                }
-            });
         }
-
         return rawView;
     } //getView()
-
 
 } //ItemsAdapter
--- app/src/main/java/il/ac/huji/todolist/TodoListManagerActivity.java
+++ app/src/main/java/il/ac/huji/todolist/TodoListManagerActivity.java
@@ -1,72 +1,157 @@
 package il.ac.huji.todolist;
-import android.content.res.Resources;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.EditText;
+import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.ArrayAdapter;
-import java.util.Arrays;
+import android.widget.TextView;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 
 public class TodoListManagerActivity extends ActionBarActivity {
-    EditText m_EdtNewItem;
-    ArrayAdapter<String> m_TodoAdapter;
+    public static final String ITEM_NAME = "0";
+    public static final String DAY = "1";
+    public static final String MONTH = "2";
+    public static final String YEAR = "3";
+    public static final int DELETE_OPTION = 0;
+    public static final int CALL_OPTION = 1;
+    public static final int ADD_REQ_CODE = 0;
+    public static final String CALL_STRING_REGEX = "\\s*\\d[\\d-]*\\s*";
+
+    ArrayList<TodoItem> m_ItemsArrayList;
+    ItemsAdapter m_TodoAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                                  WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list_manager);
         prepareListAdapter();
     }
 
     private void prepareListAdapter(){
-        Resources res;
         ListView lstTodoItems;
-        String[] todoItemsStrArr;
-        ArrayList<String> itemsArrayList;
 
-        res = getResources();
         lstTodoItems = (ListView) findViewById(R.id.lstTodoItems);
-        m_EdtNewItem = (EditText) findViewById(R.id.edtNewItem);
-        todoItemsStrArr = res.getStringArray(R.array.lstTodoItems);
-        itemsArrayList = new ArrayList<>(Arrays.asList(todoItemsStrArr));
-        m_TodoAdapter = new ItemsAdapter(
-                this, android.R.layout.simple_list_item_1, itemsArrayList);
+        m_ItemsArrayList = new ArrayList<>();
+        m_TodoAdapter = new ItemsAdapter(this,
+                                         R.layout.list_raw,
+                                         m_ItemsArrayList);
+
         lstTodoItems.setAdapter(m_TodoAdapter);
+        registerForContextMenu(lstTodoItems);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_todo_list_manager, menu);
+        getMenuInflater().inflate(R.menu.menu_add_new_todo_item, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-// Handle action bar item clicks here. The action bar will
-// automatically handle clicks on the Home/Up button, so long
-// as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-//noinspection SimplifiableIfStatement
         if (id == R.id.menuItemAdd) {
-            String itemName = m_EdtNewItem.getText().toString();
-            if (itemName.length() > 0){ // Add item to the list only if its length positive
-                m_TodoAdapter.add(itemName);
-                m_EdtNewItem.setText("");
-                return true;
-            } else {
-                return false;
-            }
+            Intent intent = new Intent(this, AddNewTodoItemActivity.class);
+            startActivityForResult(intent, ADD_REQ_CODE);
         }
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onActivityResult(
+            int reqCode, int resCode, Intent data) {
+        if (data == null){
+            return;
+        }
+        switch (reqCode) {
+            case ADD_REQ_CODE:
+                String itemName = data.getStringExtra(ITEM_NAME);
+                if (itemName == null){
+                    return;
+                }
+                Calendar date = new GregorianCalendar();
 
+                date.set(data.getIntExtra(YEAR, 0),
+                         data.getIntExtra(MONTH, 0),
+                         data.getIntExtra(DAY, 0));
+
+                m_TodoAdapter.add(new TodoItem(itemName, date));
+        }
+    }
+
+    /**
+     * @param str the string to check
+     * @return true iff the string is like the following formats: "call 123456789"
+     * or "caLL 123456-789" or "Call 123-456-789" etc.
+     */
+    private boolean isCallFormat(String str){
+        String callString;
+
+        str = str.trim().toLowerCase();
+        callString = getString(R.string.call).trim().toLowerCase();
+        return (str.matches(callString + CALL_STRING_REGEX));
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu,
+                                    View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        TodoItem item;
+        AdapterView.AdapterContextMenuInfo info;
+        String itemName;
+        TextView menuTitle;
+
+        //Set the header title based on context
+        info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        item = m_TodoAdapter.getItem(info.position);
+        itemName = item.getItemName();
+
+        //Set the title of the menu
+        menuTitle = new TextView(this);
+        menuTitle.setText(itemName);
+        menuTitle.setGravity(Gravity.CENTER);
+        menuTitle.setTextAppearance(this, android.R.style.TextAppearance_Large);
+        menu.setHeaderView(menuTitle);
+
+        //Prepare the menu and hide/unhide "call" option
+        menu.add(Menu.NONE, DELETE_OPTION , Menu.NONE, R.string.delete_item);
+        if (isCallFormat(itemName)) {
+            menu.add(Menu.NONE, CALL_OPTION , Menu.NONE, itemName.trim());
+        }
+    }
+
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info =
+                (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        switch (item.getItemId()){
+            case DELETE_OPTION:
+                m_ItemsArrayList.remove(info.position);
+                m_TodoAdapter.notifyDataSetChanged();
+                break;
+            case CALL_OPTION: //If we reached here then the item have the correct "call" format
+                String callNumber = m_ItemsArrayList.get(info.position).getItemName().trim();
+                callNumber = callNumber.substring(callNumber.indexOf(' ')).trim();
+                Intent dial = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + callNumber));
+                startActivity(dial);
+                break;
+        }
+        return true;
+    }
 }
